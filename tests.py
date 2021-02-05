60c0e90ba44ef1f8be3bba6aef7859ae2f16ce54
BeckyLashua <beckylashua@gmail.com>
2021-02-05 12:30:25 -0500

    ALL BUGS FOUND!

--- tests.py
+++ tests.py
@@ -4,18 +4,22 @@ import unittest
 
 class TestCase(unittest.TestCase):
     def test_1(self):
-        prefixes = [50, 51, 55, 56, 2220, 2221, 2720, 2721, 33, 34, 35, 37, 38]
+        prefixes = [4, 50, 51, 55, 56, 2220, 2221, 2720, 2721, 33, 34, 35, 37, 38]
         lengths = [14, 15, 16, 17]
-        tests_to_generate = 10000
+        tests_to_generate = 500000
         for i in range(tests_to_generate):
-            for prefix in range(len(prefixes)):
-                for length in range(len(lengths)):
-                    card = ''
-                    card = card + ''.join(str(prefixes[prefix]))
-                    remaining = lengths[length] - len(card)
-                    for dig in range(remaining):
-                        card = card + ''.join(str(random.randint(0, 9)))
-                    credit_card_validator(card)
+            # Pick a random prefix from prefix array
+            prefix = random.choice(prefixes)
+            # pick a random length from lenths array
+            length = random.choice(lengths)
+            # add prefix to card variable
+            card = ''
+            card = card + ''.join(str(prefix))
+            # add random digits for remaining length of card
+            remaining = length - len(card)
+            for dig in range(remaining):
+                card = card + ''.join(str(random.randint(0, 9)))
+            credit_card_validator(card)
 
 if __name__ == '__main__':
     unittest.main()
